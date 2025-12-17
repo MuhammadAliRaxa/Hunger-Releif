9ad0a55c2c3f27d74b1b60cc86c1d723c5eb9a42
MuhammadAliRaxa <muhammadaliraza7427@gmail.com>
2025-12-17 17:55:38 +0500

    Donee

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import flutter_inappwebview_ios
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,9 +8,6 @@ import flutter_inappwebview_ios
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    if let flutterViewController = window?.rootViewController as? FlutterViewController {
-      InAppWebViewFlutterPlugin.register(with: flutterViewController.registrar(forPlugin: "InAppWebViewFlutterPlugin")!)
-    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
